b3a2a04b3ccbe48b6aa49f0d05c30f60f3ee98e1
Lskyz <163709104+Lskyz@users.noreply.github.com>
2025-08-01 00:12:15 +0900

    WebViewStateModel.swift 업데이트

--- web/web/WebViewStateModel.swift
+++ web/web/WebViewStateModel.swift
@@ -5,4 +5,12 @@ class WebViewStateModel: ObservableObject {
     @Published var canGoForward = false
     @Published var currentURL: URL?
     @Published var bookmarks: [URL] = []
+
+    func goBack() {
+        NotificationCenter.default.post(name: NSNotification.Name("WebViewGoBack"), object: nil)
+    }
+
+    func goForward() {
+        NotificationCenter.default.post(name: NSNotification.Name("WebViewGoForward"), object: nil)
+    }
 }
