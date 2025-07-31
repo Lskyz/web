e7f7e9cf026aa3892084192005b8b1aab3304ec3
Lskyz <163709104+Lskyz@users.noreply.github.com>
2025-08-01 00:45:50 +0900

    CustomWebView.swift 업데이트

--- web/web/CustomWebView.swift
+++ web/web/CustomWebView.swift
@@ -4,72 +4,107 @@ import AVFoundation
 
 struct CustomWebView: UIViewRepresentable {
     @ObservedObject var stateModel: WebViewStateModel
-
+    
     func makeUIView(context: Context) -> WKWebView {
         configureAudioSessionForMixing()
+        
         let config = WKWebViewConfiguration()
         config.allowsInlineMediaPlayback = true
         config.allowsPictureInPictureMediaPlayback = true
         config.mediaTypesRequiringUserActionForPlayback = []
-
+        
         let webView = WKWebView(frame: .zero, configuration: config)
         webView.allowsBackForwardNavigationGestures = true
+        
+        // pull to refresh 연결
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(context.coordinator, action: #selector(Coordinator.handleRefresh(_:)), for: .valueChanged)
+        webView.scrollView.refreshControl = refreshControl
+        
         webView.navigationDelegate = context.coordinator
-
+        
         if let url = stateModel.currentURL {
             webView.load(URLRequest(url: url))
         }
+        
+        // NotificationCenter 등록
+        NotificationCenter.default.addObserver(context.coordinator,
+                                               selector: #selector(Coordinator.goBack),
+                                               name: NSNotification.Name("WebViewGoBack"),
+                                               object: nil)
+        NotificationCenter.default.addObserver(context.coordinator,
+                                               selector: #selector(Coordinator.goForward),
+                                               name: NSNotification.Name("WebViewGoForward"),
+                                               object: nil)
+        NotificationCenter.default.addObserver(context.coordinator,
+                                               selector: #selector(Coordinator.reloadWebView),
+                                               name: NSNotification.Name("WebViewReload"),
+                                               object: nil)
+        
         return webView
     }
-
+    
     func updateUIView(_ uiView: WKWebView, context: Context) {
         if let url = stateModel.currentURL, uiView.url != url {
             uiView.load(URLRequest(url: url))
         }
     }
-
+    
     func dismantleUIView(_ uiView: WKWebView, coordinator: Coordinator) {
         uiView.stopLoading()
         deactivateAudioSession()
+        NotificationCenter.default.removeObserver(coordinator)
     }
-
-    func makeCoordinator() -> Coordinator { Coordinator(self) }
-
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     // MARK: Audio Session Helpers
-
+    
     private func configureAudioSessionForMixing() {
         let session = AVAudioSession.sharedInstance()
         try? session.setCategory(.playback, options: [.mixWithOthers])
         try? session.setActive(true, options: [])
     }
-
+    
     private func deactivateAudioSession() {
         let session = AVAudioSession.sharedInstance()
         try? session.setActive(false, options: [.notifyOthersOnDeactivation])
     }
-
+    
     class Coordinator: NSObject, WKNavigationDelegate {
         var parent: CustomWebView
         weak var webView: WKWebView?
-
+        
         init(_ parent: CustomWebView) {
             self.parent = parent
             super.init()
-            NotificationCenter.default.addObserver(self,
-                selector: #selector(goBack), name: .init("WebViewGoBack"), object: nil)
-            NotificationCenter.default.addObserver(self,
-                selector: #selector(goForward), name: .init("WebViewGoForward"), object: nil)
         }
-
-        @objc func goBack() { webView?.goBack() }
-        @objc func goForward() { webView?.goForward() }
-
+        
+        @objc func goBack() {
+            webView?.goBack()
+        }
+        
+        @objc func goForward() {
+            webView?.goForward()
+        }
+        
+        @objc func reloadWebView() {
+            webView?.reload()
+        }
+        
+        @objc func handleRefresh(_ sender: UIRefreshControl) {
+            webView?.reload()
+            sender.endRefreshing()
+        }
+        
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             self.webView = webView
             parent.stateModel.canGoBack = webView.canGoBack
             parent.stateModel.canGoForward = webView.canGoForward
             parent.stateModel.currentURL = webView.url
-
+            
             // 모든 media 요소 음소거
             let script = """
             [...document.querySelectorAll('video'), ...document.querySelectorAll('audio')].forEach(media => {
@@ -87,7 +122,7 @@ struct CustomWebView: UIViewRepresentable {
             """
             webView.evaluateJavaScript(script)
         }
-
+        
         deinit {
             NotificationCenter.default.removeObserver(self)
         }
