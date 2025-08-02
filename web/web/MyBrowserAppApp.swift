5c08ac9d8b95a08e4445c614d3f53a8c9db26453
Lskyz <163709104+Lskyz@users.noreply.github.com>
2025-08-02 10:46:18 +0900

    MyBrowserAppApp.swift 업데이트

--- web/web/MyBrowserAppApp.swift
+++ web/web/MyBrowserAppApp.swift
@@ -5,7 +5,7 @@ struct MyBrowserAppApp: App {
     
     // ✅ 앱 시작 시 무음 오디오 재생을 통한 오디오 세션 활성화
     init() {
-        AVPSessionManager.shared.startSilentPlayback()
+        SilentAudioPlayer.shared.startSilentPlayback()
     }
 
     var body: some Scene {
