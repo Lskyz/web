568bc464e768d026f77045913acfff366b1a970d
Lskyz <163709104+Lskyz@users.noreply.github.com>
2025-09-21 00:29:35 +0900

    Update BFCacheSwipeTransition.swift

--- web/web/BFCacheSwipeTransition.swift
+++ web/web/BFCacheSwipeTransition.swift
@@ -1418,8 +1418,8 @@ extension BFCacheTransitionSystem {
             enableAnchorRestore: true,
             enableFinalVerification: true,
             savedContentHeight: max(captureData.actualScrollableSize.height, captureData.contentSize.height),
-            step1RenderDelay: 0.1,
-            step2RenderDelay: 0.2,
+            step1RenderDelay: 0.2,
+            step2RenderDelay: 0.1,
             step3RenderDelay: 0.1,
             step4RenderDelay: 0.2
         )
