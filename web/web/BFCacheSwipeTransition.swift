05941198fbb667d2b6cd5058101bc3e7e2e167a2
Lskyz <163709104+Lskyz@users.noreply.github.com>
2026-02-23 20:57:33 +0900

    chore: remove Swift files under web/web

--- web/web/BFCacheSwipeTransition.swift
+++
@@ -1,6 +0,0 @@
-//  BFCacheSwipeTransition.swift
-//  📦 이 파일의 코드는 다음 파일들로 분리되었습니다:
-//  - BFCacheSnapshot.swift  : BFCacheSnapshot struct + 4단계 복원 시스템
-//  - BFCacheJS.swift        : JavaScript 생성 코드 (Step 1~4 스크립트)
-//  - BFCacheCapture.swift   : BFCacheTransitionSystem 캡처 extension
-//  - BFCache UI.swift       : BFCacheTransitionSystem 클래스 + 제스처 시스템
